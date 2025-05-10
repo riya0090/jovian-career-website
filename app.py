3189f2cda449ce6596c38a33b82d169fa6e452bf
riya0090 <162570457+riya0090@users.noreply.github.com>
2025-05-10 11:18:01 +0000

    Added dynamic data and api route

--- app.py
+++ app.py
@@ -1,8 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 app = Flask(__name__)
-@app.route("/")
+JOBS=[{
+     'id':'1',
+     'title': 'Data Analyst',
+     'location': 'Bengaluru,India',
+     'salary': 'Rs 10,00,000'
+},
+{
+     'id':'2',
+     'title': 'Frontend Engineer',
+     'location': 'Delhi,India',
+     'salary': 'Rs 15,00,000'
+},
+{
+     'id':'3',
+     'title': 'Backend Engineer',
+     'location': 'Remote,India',
+},
+{
+     'id':'4',
+     'title': 'DataScientist Engineer',
+     'location': 'San Fransisco',
+     'salary': '$12,0,000'
+}
+]
+@app.route("/")# we are create this route which says that when we go to the home page we want to return the hello world
 def hello_world():
-     return render_template('home.html')
-print(__name__)
+     return render_template('home.html', jobs=JOBS, company_name='Jovian')#we are returning the home.html file and we are passing the jobs list also the company name which is dynamic to the home.html file 
+@app.route("/api/jobs")
+def list_jobs():
+     return jsonify(JOBS)
+ 
 if __name__ == "__main__":
      app.run(host= '0.0.0.0', debug=True)
